06218a06e33b00700806ffb8f6cf85b2c8dbd25a
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-05-07 12:58:37 -0500

    Refactor to use port specified in env file for proxying

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,39 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { join } from "node:path";
 import { cwd } from "node:process";
 import react from "@vitejs/plugin-react";
 
 // Read about the Vite config here: https://vitejs.dev/config/
-export default defineConfig({
-  root: "frontend",
-  build: {
-    outDir: join(cwd(), "build/frontend"),
-    emptyOutDir: true,
-  },
-  plugins: [react()],
-  server: {
-    // Proxy API requests to the backend, to make them appear as if
-    // they are coming from the same origin.
-    // Read up here: https://vitejs.dev/config/server-options.html#server-proxy
-    proxy: {
-      // Shorthand syntax
-      // http://localhost:5173/ping -> http://localhost:3000/ping
-      "/ping": "http://localhost:3000",
-      "/auth": "http://localhost:3000",
-      "/user": "http://localhost:3000",
-      "/list": "http://localhost:3000",
-
-      // Full syntax
-      // http://localhost:5173/api/foo -> http://localhost:3000/foo
-      // "/api": {
-      //   target: "http://localhost:3000",
-      //   changeOrigin: true,
-      //   rewrite: (path) => path.replace(/^\/api/, ""),
-      // },
+export default defineConfig(({ mode }) => {
+  const { PORT } = loadEnv(mode, cwd(), "");
+  const apiProxy = `http://localhost:${PORT}`;
+  return {
+    root: "frontend",
+    build: {
+      outDir: join(cwd(), "build/frontend"),
+      emptyOutDir: true,
     },
-  },
+    plugins: [react()],
+    server: {
+      // Proxy API requests to the backend, to make them appear as if
+      // they are coming from the same origin.
+      // Read up here: https://vitejs.dev/config/server-options.html#server-proxy
+      proxy: {
+        // Shorthand syntax
+        // http://localhost:5173/ping -> http://localhost:3000/ping
+        "/ping": apiProxy,
+        "/auth": apiProxy,
+        "/user": apiProxy,
+        "/list": apiProxy,
+
+        // Full syntax
+        // http://localhost:5173/api/foo -> http://localhost:3000/foo
+        // "/api": {
+        //   target: "http://localhost:3000",
+        //   changeOrigin: true,
+        //   rewrite: (path) => path.replace(/^\/api/, ""),
+        // },
+      },
+    },
+  };
 });
