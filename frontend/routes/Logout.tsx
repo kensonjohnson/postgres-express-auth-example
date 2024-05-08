9e55703d8993f0b4d4dd47afc9e58787098b1cb4
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-05-08 11:20:24 -0500

    Move logout form to a component, fix styles

--- frontend/routes/Logout.tsx
+++ frontend/routes/Logout.tsx
@@ -1,7 +1,25 @@
-import { redirect } from "react-router-dom";
+import { Form, redirect } from "react-router-dom";
 import { authProvider } from "../providers/auth-provider";
 
 export async function action() {
   await authProvider.logout();
   return redirect("/");
+}
+
+export function Logout() {
+  return (
+    <Form
+      className="button"
+      style={{ marginLeft: "auto" }}
+      action="/logout"
+      method="post"
+    >
+      <button
+        style={{ background: "none", border: 0, fontSize: "inherit" }}
+        type="submit"
+      >
+        Logout
+      </button>
+    </Form>
+  );
 }
