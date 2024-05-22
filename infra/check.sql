45dd28be8d99f6382505d971b9e13d5be013f67d
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-05-22 11:56:51 -0500

    Convert init.sql to idempotent sql

--- infra/check.sql
+++ infra/check.sql
@@ -1,8 +1,9 @@
+-- No longer used, but kept for reference
 SELECT
     COUNT(*) AS table_count
 FROM
     information_schema.tables
 WHERE
     table_schema = 'public'
-    AND table_name IN ('list', 'task', 'users', 'session');
+    AND table_name IN ('list', 'task', 'users', 'session', 'conversation', 'message');
 
--- infra/setup_database.js
+++ infra/setup_database.js
@@ -15,49 +15,17 @@ const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-const check = await checkTablesExist();
+const sql = readFileSync("infra/init.sql").toString();
 
-switch (check.rows[0].table_count) {
-  case "0":
-    console.log("Creating tables...");
-    await createTables();
-    console.log("✅ " + colorize("Tables created successfully."));
-    exit();
-    break;
-  case "1":
-  case "2":
-  case "3":
-    console.log(
-      "❗ " + colorize("Bad configuration found. Recreating tables...")
-    );
-    await deleteTables();
-    await createTables();
-    console.log("✅ " + colorize("Tables recreated successfully. Exiting."));
-    exit();
-    break;
-  case "4":
-    console.log("✅ " + colorize("Tables already created. Exiting."));
-    exit();
-    break;
-  default:
-    console.error("❌ " + colorize("Unexpected error encountered. Exiting."));
+pool.query(sql, (error) => {
+  if (error) {
+    console.error("❌ " + colorize("Error creating tables: " + error));
+    pool.end();
     exit(1);
-}
-
-function checkTablesExist() {
-  const sql = readFileSync("infra/check.sql").toString();
-  return pool.query(sql);
-}
-
-function createTables() {
-  const sql = readFileSync("infra/init.sql").toString();
-  return pool.query(sql);
-}
-
-function deleteTables() {
-  const sql = readFileSync("infra/delete.sql").toString();
-  return pool.query(sql);
-}
+  }
+  console.log("✅ " + colorize("Tables created successfully."));
+  exit();
+});
 
 function exit(code = 0) {
   pool.end();
