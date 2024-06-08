eae432ec3b499dc5e62c9ea486b3c63072fa8972
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-06-08 13:16:42 -0500

    Add recalculating user credit balance

--- src/controllers/billing-controller.ts
+++ src/controllers/billing-controller.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from "express";
 import { db } from "../drizzle/db.js";
 import { CreditTable, DebitTable, UserTable } from "../drizzle/schema.js";
-import { eq } from "drizzle-orm";
+import { and, eq, gt, ne, sum } from "drizzle-orm";
 
 export async function getBalance(req: Request, res: Response) {
   const userId = req.user!.id;
@@ -19,9 +19,49 @@ export async function getBalance(req: Request, res: Response) {
 export async function addCredits(req: Request, res: Response) {
   const userId = req.user!.id;
   try {
-    await db.insert(CreditTable).values({ user_id: userId, amount: 10 });
+    const sixtyDaysAgo = new Date();
+    sixtyDaysAgo.setDate(sixtyDaysAgo.getDate() - 60);
 
-    res.json({ message: "Credit successful", amount: 10 });
+    // Calculate the user's balance, updating it in this user's account.
+    const { balance } = await db.transaction(async (tx) => {
+      await db.insert(CreditTable).values({ user_id: userId, amount: 10 });
+      const credits = await tx
+        .select({ credit: sum(CreditTable.amount) })
+        .from(CreditTable)
+        .where(
+          and(
+            eq(CreditTable.user_id, userId),
+            gt(CreditTable.created_on, sixtyDaysAgo)
+          )
+        );
+      const debits = await tx
+        .select({ debit: sum(DebitTable.amount) })
+        .from(DebitTable)
+        .where(
+          and(
+            eq(DebitTable.user_id, userId),
+            gt(DebitTable.created_on, sixtyDaysAgo)
+          )
+        );
+
+      const balance =
+        Number(credits.at(0)?.credit) - Number(debits.at(0)?.debit);
+
+      await tx
+        .update(UserTable)
+        .set({ credit_balance: balance })
+        .where(eq(UserTable.id, userId));
+
+      return {
+        balance,
+      };
+    });
+
+    res.json({
+      message: "Credit successful",
+      amount: 10,
+      new_balance: balance,
+    });
   } catch (error) {
     req.log.error(error);
     res.end().status(500);
@@ -32,9 +72,45 @@ export async function removeCredits(req: Request, res: Response) {
   const userId = req.user!.id;
 
   try {
-    await db.insert(DebitTable).values({ user_id: userId, amount: 1 });
+    const sixtyDaysAgo = new Date();
+    sixtyDaysAgo.setDate(sixtyDaysAgo.getDate() - 60);
 
-    res.json({ message: "Debit successful", amount: 1 });
+    // Calculate the user's balance, updating it in this user's account.
+    const { balance } = await db.transaction(async (tx) => {
+      await db.insert(DebitTable).values({ user_id: userId, amount: 1 });
+      const credits = await tx
+        .select({ credit: sum(CreditTable.amount) })
+        .from(CreditTable)
+        .where(
+          and(
+            eq(CreditTable.user_id, userId),
+            gt(CreditTable.created_on, sixtyDaysAgo)
+          )
+        );
+      const debits = await tx
+        .select({ debit: sum(DebitTable.amount) })
+        .from(DebitTable)
+        .where(
+          and(
+            eq(DebitTable.user_id, userId),
+            gt(DebitTable.created_on, sixtyDaysAgo)
+          )
+        );
+
+      const balance =
+        Number(credits.at(0)?.credit) - Number(debits.at(0)?.debit);
+
+      await tx
+        .update(UserTable)
+        .set({ credit_balance: balance })
+        .where(eq(UserTable.id, userId));
+
+      return {
+        balance,
+      };
+    });
+
+    res.json({ message: "Debit successful", amount: 1, new_balance: balance });
   } catch (error) {
     req.log.error(error);
     res.end().status(500);
